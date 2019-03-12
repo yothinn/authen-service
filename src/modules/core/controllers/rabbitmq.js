4a8cd6b428fe89abe9fdb2d31eec5769e9e7529c
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-03-12 15:30:42 +0700

    as

--- src/modules/core/controllers/rabbitmq.js
+++ src/modules/core/controllers/rabbitmq.js
@@ -1,6 +1,6 @@
 const amqp = require('amqplib/callback_api');
 
-const AMQP_URL = 'amqp://localhost:5672';
+const AMQP_URL = process.env.AMQP_URL;
 
 const ON_DEATH = require('death');
 
@@ -23,26 +23,26 @@ module.exports.publish = function (ex, msgKey, msgPayload) {
     });
 }
 module.exports.consume = function (ex, qname, msgKey, invkFn) {
-    var msg = 'test'
-    invkFn(msg);
+    // var msg = 'test'
+    // invkFn(msg);
     amqp.connect(AMQP_URL, function (err, conn) {
-    //     conn.createChannel(function (err, ch) {
-    //         ch.assertExchange(ex, 'direct', { durable: true })
-    //         ch.assertQueue(qname, { exclusive: false }, function (err, q) {
-    //             ch.bindQueue(q.queue, ex, msgKey);
-    //             ch.consume(q.queue, function (msg) {
-    //                 invkFn(msg);
+        conn.createChannel(function (err, ch) {
+            ch.assertExchange(ex, 'direct', { durable: true })
+            ch.assertQueue(qname, { exclusive: false }, function (err, q) {
+                ch.bindQueue(q.queue, ex, msgKey);
+                ch.consume(q.queue, function (msg) {
+                    invkFn(msg);
 
-    //                 ON_DEATH(function (signal, err) {
-    //                     console.log('clear');
-    //                     setTimeout(function () {
-    //                         conn.close();
-    //                         process.emit(0);
-    //                     })
-    //                 })
-    //             }, { noAck: true })
-    //         })
-    //     })
+                    ON_DEATH(function (signal, err) {
+                        console.log('clear');
+                        setTimeout(function () {
+                            conn.close();
+                            process.emit(0);
+                        })
+                    })
+                }, { noAck: true })
+            })
+        })
 
     })
 }
