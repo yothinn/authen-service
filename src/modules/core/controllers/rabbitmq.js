82a1d7254216d8f60f93876535c902e6c2f3731d
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-03-04 19:09:20 +0700

    fix issue of mq consume

--- src/modules/core/controllers/rabbitmq.js
+++ src/modules/core/controllers/rabbitmq.js
@@ -1,75 +1,102 @@
-const amqp = require('amqplib/callback_api');
+const amqp = require("amqplib/callback_api");
 
-const AMQP_URL = process.env.AMQP_URL || 'amqp://localhost';
+const AMQP_URL = process.env.AMQP_URL || "amqp://localhost";
 
-const ON_DEATH = require('death');
+const ON_DEATH = require("death");
 
-
-module.exports.publish = function (ex, msgKey, msgPayload) {
-    amqp.connect(AMQP_URL, function (err, conn) {
-        conn.createChannel(function (err, ch) {
-            ch.assertExchange(ex, 'direct', { durable: true });
-            ch.publish(ex, msgKey, Buffer.from(msgPayload));
-            console.log(" [X] Sent '%s' ", msgKey);
-            return '';
-
-        });
-        ON_DEATH(function (signal, err) {
-            console.log('clear');
-            setTimeout(function () {
-                conn.close();
-                process.emit(0);
-            }, 500);
-        })
+module.exports.publish = function(ex, msgKey, msgPayload) {
+  amqp.connect(AMQP_URL, function(err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return;
+    }
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
+      }
     });
-}
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+      return;
+    });
 
+    console.log("[AMQP] connected");
 
-module.exports.consume = function (ex, qname, msgKey, invkFn) {
-    // var msg = 'test'
-    // invkFn(msg);
-    amqp.connect(AMQP_URL, function (err, conn) {
-        conn.createChannel(function (err, ch) {
-            console.log("amqp connected");
+    conn.createChannel(function(err, ch) {
+      if (err) return;
+      ch.on("error", function(err) {
+        console.error("[AMQP] channel error", err.message);
+      });
+      ch.on("close", function() {
+        console.log("[AMQP] channel closed");
+      });
+      ch.assertExchange(ex, "direct", { durable: true });
+      ch.publish(ex, msgKey, Buffer.from(msgPayload));
+    //   console.log(" [X] Sent '%s' ", msgKey);
+      return "";
+    });
+    ON_DEATH(function(signal, err) {
+      console.log("clear");
+      setTimeout(function() {
+        conn.close();
+        process.emit(0);
+      }, 500);
+    });
+  });
+};
 
+module.exports.consume = function(ex, qname, msgKey, invkFn) {
+  amqp.connect(AMQP_URL, function(err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return;
+    }
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
+      }
+    });
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+      return;
+    });
 
+    console.log("[AMQP] connected");
 
-            ch.assertExchange(ex, 'direct', { durable: true })
-            // ch.assertQueue(qname, { exclusive: false }, function (err, q) {
+    conn.createChannel(function(err, ch) {
+      if (err) return;
+      ch.on("error", function(err) {
+        console.error("[AMQP] channel error", err.message);
+      });
+      ch.on("close", function() {
+        console.log("[AMQP] channel closed");
+      });
+      ch.assertExchange(ex, "direct", { durable: true });
+      // ch.assertQueue(qname, { exclusive: false }, function (err, q) {
 
+      // })
+      ch.assertQueue(qname, {
+        exclusive: false
+      });
 
+      ch.bindQueue(qname, ex, msgKey);
 
-            // })
-            ch.assertQueue(qname, {
-                exclusive: false
+      ch.consume(
+        qname,
+        function(msg) {
+          // console.log("======");
+          invkFn(msg);
+
+          ON_DEATH(function(signal, err) {
+            // console.log("clear");
+            setTimeout(function() {
+              conn.close();
+              process.emit(0);
             });
-
-            ch.bindQueue(qname, ex, msgKey);
-            
-
-            ch.consume(qname, function (msg) {
-                // console.log("======");
-                invkFn(msg);
-
-                ON_DEATH(function (signal, err) {
-                    console.log('clear');
-                    setTimeout(function () {
-                        conn.close();
-                        process.emit(0);
-                    })
-                    
-                })
-
-
-
-            }, { noAck: true })
-
-
-        })
-
-    })
-}
-
-
-
-
+          });
+        },
+        { noAck: true }
+      );
+    });
+  });
+};
